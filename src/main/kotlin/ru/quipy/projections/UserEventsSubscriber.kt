3270eb13bcd0c7fcdadae54d858e90fe9d925b47
n.kovregin <nikolai.kovregin@hotmail.com>
2023-11-12 21:04:17 +0300

    feat: user projection

---
+++ src/main/kotlin/ru/quipy/projections/UserEventsSubscriber.kt
@@ -0,0 +1,48 @@
+package ru.quipy.projections
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
+import ru.quipy.api.*
+import ru.quipy.api.user.UserCreatedEvent
+import ru.quipy.streams.AggregateSubscriptionsManager
+import java.util.*
+import javax.annotation.PostConstruct
+
+
+@Service
+class UserEventsSubscriber(
+    private val userCacheRepository: UserCacheRepository,
+) {
+    val logger: Logger = LoggerFactory.getLogger(UserEventsSubscriber::class.java)
+
+    @Autowired
+    lateinit var subscriptionsManager: AggregateSubscriptionsManager
+    @PostConstruct
+    fun init() {
+        subscriptionsManager.createSubscriber(UserAggregate::class, "transactions::user-cache") {
+
+            `when`(UserCreatedEvent::class) { event ->
+                userCacheRepository.save(User(event.userId, event.userName, event.userNickName, event.userPassword))
+                logger.info("User created: {}", event.userId)
+            }
+        }
+    }
+}
+
+@Document("transactions::user-cache")
+data class User(
+    @Id
+    val userId: UUID,
+    val userName: String,
+    val userNickName: String,
+    val userPassword: String
+)
+
+@Repository
+interface UserCacheRepository: MongoRepository<User, UUID>
